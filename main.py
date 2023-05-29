c2184fd7a1f21dc267fce7c55fd2ec8501e8e3e4
ZaidSalahuddin <59754860+ZaidSalahuddin@users.noreply.github.com>
2023-05-29 18:58:49 -0400

    got the libraries working ig not much change

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import numpy as np
-import opencv as cv2
+import cv2
+from PIL import ImageGrab
 
-bbox = ()
+bbox = (0,0,1600,1200)
+
+# function to screen shot
+def screenshot():
+    image = np.array(ImageGrab.grab(bbox=bbox))
+    return image
+
+def get_board():
+    pass
+
+print('working')
+screen = screenshot()
+cv2.imshow("screenshot",screen)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
