2d19554a6b954f6df69e1962a625b2bc128fa405
ZaidSalahuddin <59754860+ZaidSalahuddin@users.noreply.github.com>
2023-05-29 23:08:10 -0400

    smol change to filters, still figuring it out

--- main.py
+++ main.py
@@ -2,18 +2,31 @@ import numpy as np
 import cv2
 from PIL import ImageGrab
 
-bbox = (0,0,1600,1200)
+bbox = (0,0,1920,1200)
 
 # function to screen shot
 def screenshot():
     image = np.array(ImageGrab.grab(bbox=bbox))
     return image
 
-def get_board():
-    pass
+#process to isolate the chessboard from screenshot
+def get_board(screen):
+    #convert to greyscale
+    img = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+
+    #edge detection
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    img = cv2.Canny(img, 50, 150)
+
+    #masks
+    white_mask = np.all(input == 255, 2).astype(np.uint8)*255  # cv2.inRange(input, (255, 255, 255), (255, 255, 255))
+    black_mask = np.all(input == 230, 2).astype(np.uint8)*255  # gray_mask = cv2.inRange(input, (230, 230, 230), (230, 230, 230))
+
+    return img
 
 print('working')
 screen = screenshot()
-cv2.imshow("screenshot",screen)
+board = get_board(screen)
+cv2.imshow("screenshot",board)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
