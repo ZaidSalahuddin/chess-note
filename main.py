adb54d70a1ba9852d8e146763997324e269d1308
ZaidSalahuddin <59754860+ZaidSalahuddin@users.noreply.github.com>
2023-05-31 00:03:34 -0400

    got the rect part to wrok i guess

--- main.py
+++ main.py
@@ -9,9 +9,7 @@ def distance(x1,y1,x2,y2):
     x_diff = x2-x1
     y_diff = y2-y1
     x_diff_sq = np.abs(x_diff^2)
-    print("xdiff",x_diff_sq)
     y_diff_sq = np.abs(y_diff^2)
-    print("ydiff",y_diff_sq)
     dist = np.sqrt(x_diff_sq+y_diff_sq)
     return dist
 
@@ -58,22 +56,33 @@ def get_board(screen):
     
     if ret == True:
         #draw the chessboard area on the mask
-        mask = cv2.drawChessboardCorners(mask, corner_dim, corners, ret)
+        #mask = cv2.drawChessboardCorners(mask, corner_dim, corners, ret)
         
         #extend the mask to the 8x8 sides
         #use corners 0 and 8 for top right square
         #use index -1 and -9 for bottom left
         #print("corners shape",corners)
+        #makes the array 2d from 3d
         corners_2d = corners
         corners_2d = np.reshape(corners_2d,(49,2))
 
-        side_length_top_left = distance(int(corners_2d[0,0]), int(corners_2d[0,1]), int(corners_2d[8,0]), int(corners_2d[8,1]))
-        print("side legt top", side_length_top_left)
+        #gets the distance from the corners of the corner squares and finds a point 1 diagonal away
+        side_length_top = distance(int(corners_2d[0,0]), int(corners_2d[0,1]), int(corners_2d[8,0]), int(corners_2d[8,1]))
+        side_length_bottom = distance(int(corners_2d[-1,0]), int(corners_2d[-1,1]), int(corners_2d[-9,0]), int(corners_2d[-9,1]))
 
-        side_length_bottom_right = distance(int(corners_2d[-1,0]), int(corners_2d[-1,1]), int(corners_2d[-9,0]), int(corners_2d[-9,1]))
-        print("side legnth bottom", side_length_bottom_right)
+        top_point = (int(corners_2d[0,0]-side_length_top), int(corners_2d[0,1]-side_length_top))
+        bottom_point = (int(corners_2d[-1,0]-side_length_bottom), int(corners_2d[-1,1]-side_length_bottom))
 
-        #drawing a rectagle over it igb
+        #drawing a rectagle mask with the previous point
+        print(gray.shape)
+        rect = np.zeros(gray.shape, dtype=np.uint8)
+        mask = np.reshape(rect, rect.shape + (1,))
+        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+        mask = cv2.rectangle(mask,top_point,bottom_point,255)
+
+
+        print("mnask shape: ", mask.shape)
+
         #show the mask 
         cv2.imshow(f"mask",mask)
 
